e75ab697bf9dd4743255c3212a12b00376dcd357
Jerrytru <trujillojerry70@yahoo.com>
2023-05-21 16:07:53 -0400

    initial

---
+++ src/main/java/practice/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package practice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String generate(){
+		return "hello";
+	}
+
+}
